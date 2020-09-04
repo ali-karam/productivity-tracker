34a78361198368fac42294a3bd7157a4bd82efcb
Ali <Ali@MacBook-Pro.local>
2020-09-04 20:19:55 -0300

    Made activity a class-based component that manages the stopwatch and countdown

---
+++ src/containers/Activity/Activity.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import './Activity.css';
+
+class Activity extends Component {
+    state = {
+        timerOn: false,
+        stopwatchStart: 0,
+        stopwatchTime: 0,
+        timerTime: this.props.duration,
+        timerStart: 0
+    };
+
+    startTimer = () => {
+        this.setState({
+          timerOn: true,
+          stopwatchTime: this.state.stopwatchTime,
+          stopwatchStart: Date.now() - this.state.stopwatchTime,
+          timerTime: this.state.timerTime,
+          timerStart: this.state.timerTime
+        });
+
+        this.timer = setInterval(() => {
+            this.setState({
+                stopwatchTime: Date.now() - this.state.stopwatchStart
+            });
+
+            const newTime = this.state.timerTime - 1000;
+            if(newTime >= 0) {
+                this.setState({
+                    timerTime: newTime
+                });
+            } else {
+                clearInterval(this.timer);
+                this.setState({timerOn: false});
+            }
+        }, 1000);
+    };
+
+    stopTimer = () => {
+        this.setState({ timerOn: false });
+        clearInterval(this.timer);
+    };
+
+    displayTime = (time) => {
+        let seconds = ("0" + (Math.floor(time / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(time / 60000) % 60)).slice(-2);
+        let hours = ("0" + Math.floor(time / 3600000)).slice(-2);
+        return `${hours} : ${minutes} : ${seconds}`;
+    };
+
+    render() {
+        return (
+            <div className='Activity'>
+                <h3 className='ActivityName'>{this.props.name}</h3>
+                <p>{this.displayTime(this.state.timerTime)}</p>
+                <p>{this.displayTime(this.state.stopwatchTime)}</p>
+                <button onClick={this.startTimer}>Start</button>
+                <button onClick={this.stopTimer}>Pause</button>
+            </div>
+        );
+    }
+};
+
+export default Activity;
--- src/containers/ProductivityTracker/ProductivityTracker.js
+++ src/containers/ProductivityTracker/ProductivityTracker.js
@@ -1,55 +1,11 @@
 import React, {Component} from 'react';
-import Activity from '../../components/Activity/Activity';
+import Activity from '../Activity/Activity';
 
 class ProductivityTracker extends Component {
-    state = {
-        timerOn: false,
-        timerStart: 0,
-        timerTime: 0
-    };
-
-    startTimer = () => {
-        this.setState({
-            timerOn: true,
-            timerStart: Date.now() - this.state.timerTime
-        });
-        this.timer = setInterval(() => {
-            this.setState({
-                timerTime: Date.now() - this.state.timerStart
-            });
-        }, 1000);
-    };
-
-    stopTimer = () => {
-        this.setState({timerOn: false});
-        clearInterval(this.timer);
-    };
-
-    resetTimer = () => {
-        this.stopTimer();
-        this.setState({
-            timerStart: 0,
-            timerTime: 0
-        });
-    };
-
-    displayTime = () => {
-        const timerTime  = this.state.timerTime;
-        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
-        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
-        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
-        return `${hours} : ${minutes} : ${seconds}`;
-    }
-
     render() {
         return (
             <div>
-                <Activity 
-                    time={this.displayTime()} 
-                    startTimer={this.startTimer}
-                    stopTimer={this.stopTimer}
-                    resetTimer={this.resetTimer}
-                />
+                <Activity duration={1000 * 60 * 60 * 5} name="Study"/>
             </div>
         );
     }
