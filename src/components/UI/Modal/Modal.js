1c1de8ce1750d4d10654ab17ca8aed3ee868a29e
Ali <Ali@MacBook-Pro.local>
2020-09-05 13:51:39 -0300

    Optimized the modal to update only when the show property changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,6 +4,10 @@ import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
     render() {
         return (
             <React.Fragment>
